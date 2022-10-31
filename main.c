514c8a6791afd797e0329a79fbd15f1edcc0dca5
Julio Cesar Jauer <jcjauer@A5-JCJ.localdomain>
2022-10-31 17:18:07 -0400

    Um teste

--- main.c
+++ main.c
@@ -1,27 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "Autocomplete.h"
-/*
-int main(){
 
-    int i;
-    char palavras[55] = {"Oi. Vamos aprender a programar com a linguagem C?"};
-
-    printf("Digite seu nome: ");
-    fgets(palavras, 55, stdin);// file
-
-    i = 0;
-    // \0 -> caracter que indica o fim de string
-    // enquanto palavras[i] for diferente do fim de string...
-    while(palavras[i] != '\0'){
-        printf("%d = %d\n", i, palavras[i]);
-        i++;
-    }
-    printf("\n");
-
-    return 0;
-}
-*/
 int main() {
 
     Node *lista = NULL;
